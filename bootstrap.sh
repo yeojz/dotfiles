21a365502f58d27b65e3dc92a57353e3ab632dac
Gerald Yeo <yeo.junzhi@gmail.com>
2013-10-28 12:22:49 -0700

    refactor and bootstrapped

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#
+#	BOOTSTRAP FILE
+# 	just include this to get everything or config your settings here
+#
+
+#Environment Settings
+source ~/dotfiles/env/prompt
+source ~/dotfiles/env/virtualenv
+
+#Aliases Repository
+source ~/dotfiles/aliases/subl
+
