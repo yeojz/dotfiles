92fc6b9dfefad1063d491299e799ce01f8a25508
Gerald Yeo <gerald@fusedthought.com>
2016-06-21 23:22:37 +0800

    remove homebrew appdir + list common atom apm pkg

---
+++ install/atomfile.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+apm install atom-material-ui
+apm install devdocs
+apm install editorconfig
+apm install file-icons
+apm install git-blame
+apm install git-control
+apm install highlight-selected
+apm install language-babel
+apm install linter
+apm install linter-eslint
+apm install linter-php
+apm install linter-sass-lint
+apm install minimap
+apm install oceanic-next
+apm install pigments
+apm install project-manager
+apm install react
+apm install split-diff
